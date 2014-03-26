2a750148054cf87fa4240984594afec3ad02e0be
Andrew Pong <andrew@awp.io>
2014-03-26 16:27:37 -0700

    Emptied folder

--- wp-content/themes/striking_r/framework/info.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-	'theme_name' => 'Striking Responsive', 
-	'theme_slug' => 'strikingr',
-	'theme_version' => '0.9.9.3',
-	'required_wp_version' => '3.1',
-);
--- wp-content/themes/striking_r/sections/sidebar.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * The default template for displaying sidebar in the pages
- */
-function theme_section_sidebar(){
-	return sidebar_generator('get_sidebar',get_queried_object_id());
-}
